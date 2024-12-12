6941d5e0fc9da0ac72d0467a3113d942d6c8e000
gallardo4 <arnau.gallardo.7e7@itb.cat>
2024-12-12 17:44:35 +0100

    Commit

---
+++ ejemplosVUE3/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
